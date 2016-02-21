a57b623a5f1cfee7c214fbf57f69758dc26ec2b2
fanna <fanna993@gmail.com>
2016-02-21 17:28:49 +0100

    Add better CPU status control

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,58 @@ import Rack from "./Rack";
 
 export default class Layout extends React.Component {
   render() {
-    var racks = [
-      1,2,3,4,5,6
-    ].map((rack_id, i) => <Rack key={i} rack_id={rack_id}/>);
+    var rackList= [
+      {id: 1, servers:[
+        {id: 1, cpus:[{id: 1, active: false}, {id: 2, active:false}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 2, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 3, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 4, cpus:[{id: 1, active: false}, {id: 2, active:true}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 5, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 6, cpus:[{id: 1, active: true}, {id: 2, active: true}, {id: 3, active: false}, {id: 4, active: false}]},
+     ]},
+      {id: 2, servers:[
+        {id: 1, cpus:[{id: 1, active: false}, {id: 2, active:false}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 2, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 3, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 4, cpus:[{id: 1, active: false}, {id: 2, active:true}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 5, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 6, cpus:[{id: 1, active: true}, {id: 2, active: true}, {id: 3, active: false}, {id: 4, active: false}]},
+     ]},
+      {id: 3, servers:[
+        {id: 1, cpus:[{id: 1, active: false}, {id: 2, active:false}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 2, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 3, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 4, cpus:[{id: 1, active: false}, {id: 2, active:true}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 5, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 6, cpus:[{id: 1, active: true}, {id: 2, active: true}, {id: 3, active: false}, {id: 4, active: false}]},
+     ]},
+      {id: 4, servers:[
+        {id: 1, cpus:[{id: 1, active: false}, {id: 2, active:false}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 2, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 3, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 4, cpus:[{id: 1, active: false}, {id: 2, active:true}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 5, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 6, cpus:[{id: 1, active: true}, {id: 2, active: true}, {id: 3, active: false}, {id: 4, active: false}]},
+     ]},
+      {id: 5, servers:[
+        {id: 1, cpus:[{id: 1, active: false}, {id: 2, active:false}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 2, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 3, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 4, cpus:[{id: 1, active: false}, {id: 2, active:true}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 5, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 6, cpus:[{id: 1, active: true}, {id: 2, active: true}, {id: 3, active: false}, {id: 4, active: false}]},
+     ]},
+      {id: 6, servers:[
+        {id: 1, cpus:[{id: 1, active: false}, {id: 2, active:false}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 2, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 3, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 4, cpus:[{id: 1, active: false}, {id: 2, active:true}, {id: 3, active: true}, {id: 4, active: false}]},
+        {id: 5, cpus:[{id: 1, active: true}, {id: 2, active:false}, {id: 3, active: false}, {id: 4, active: false}]},
+        {id: 6, cpus:[{id: 1, active: true}, {id: 2, active: true}, {id: 3, active: false}, {id: 4, active: true}]},
+     ]}];
+
+    var racks = rackList.map((rack) =>
+                             <Rack key={rack.id} rack_id={rack.id} servers={rack.servers} />);
     return (
       <div>
       {racks}
@@ -14,3 +63,4 @@ export default class Layout extends React.Component {
     );
   }
 }
+
--- src/js/components/Rack/Server/CPU.js
+++ src/js/components/Rack/Server/CPU.js
@@ -2,22 +2,14 @@ import React from "react";
 
 
 export default class CPU extends React.Component {
-  constructor() {
-    super();
-    this.state = {"active": false};
-  }
-
-  changeStatus(){
-    this.setState({"active": !this.state.active});
-  }
   render() {
     var cpuStyle= {
       borderWidth: 1,
       borderStyle: 'solid',
-      backgroundColor: this.state.active ? 'lawngreen' : 'grey'
+      backgroundColor: this.props.active ? 'lawngreen' : 'grey'
     };
     return (
-      <td style={cpuStyle} onClick={this.changeStatus.bind(this)}>cpu{this.props.cpu_id}</td>
+      <td style={cpuStyle}>cpu{this.props.cpu_id}</td>
     );
   }
 }
