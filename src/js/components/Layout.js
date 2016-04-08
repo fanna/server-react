06561defef432fc1cef30d2f3ca64cf9d7861814
Stefan Mijucic <fanna993@gmail.com>
2016-04-08 11:27:27 +0200

    Remove Rack abstraction

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,61 +2,75 @@ import React from "react";
 
 import { Socket } from "phoenix-js";
 
-import Rack from "./Rack";
+import Server from "./Server";
 
-let socket = new Socket("ws://rocky-ridge-42687.herokuapp.com/socket")
+//let socket = new Socket("ws://rocky-ridge-42687.herokuapp.com/socket")
+let socket = new Socket("ws://localhost:4000/socket")
 
 socket.connect();
 
-var rackList= [
-  {id: 1, servers:[
-  {id: 1, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 2, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 3, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 4, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 5, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 6, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-]},
-{id: 2, servers:[
-  {id: 1, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 2, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 3, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 4, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 5, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 6, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-]},
-{id: 3, servers:[
-  {id: 1, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 2, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 3, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 4, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 5, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 6, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-]},
-{id: 4, servers:[
-  {id: 1, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 2, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 3, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 4, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 5, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 6, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-]},
-{id: 5, servers:[
-  {id: 1, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 2, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 3, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 4, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 5, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 6, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-]},
-{id: 6, servers:[
-  {id: 1, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 2, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 3, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 4, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 5, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-  {id: 6, cpus:[{id: 1, active: false}, {id: 2, active: false}, {id: 3, active: false}, {id: 4, active: false}]},
-]}];
+var serverList= [
+  {id: 1, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 2, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 3, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 4, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 5, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 6, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 7, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 8, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 9, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  },
+  {id: 10, cpus:[
+    {id: 1, active: false},
+    {id: 2, active: false},
+    {id: 3, active: false},
+    {id: 4, active: false}]
+  }
+]
 
 let channel = socket.channel("rooms:lobby", {})
 let json = "false"
@@ -65,19 +79,17 @@ channel.join().receive("ok", () => {
   console.log("Joined successfully")
 })
 
-var rack = 0
 var server = 0
 var cpu = 0
 var cpu_status = true
 
 channel.on("new_msg", payload => {
   cpu_status  = payload.status
-  rack = payload.rack
   server = payload.server
   cpu = payload.cpu
-  console.log("Status change on Rack: " + rack + " Server: " + server + " CPU: " + cpu);
+  console.log("Status change on Server: " + server + " CPU: " + cpu);
   console.log(cpu_status)
-  rackList[rack].servers[server].cpus[cpu].active = cpu_status;
+  serverList[server].cpus[cpu].active = cpu_status;
 })
 
 export default class Layout extends React.Component {
@@ -94,11 +106,11 @@ export default class Layout extends React.Component {
 
   render() {
 
-    var racks = rackList.map((rack) =>
-                             <Rack key={rack.id} rack_id={rack.id} servers={rack.servers} />);
+    var servers = serverList.map((server) =>
+                             <Server key={server.id} server_id={server.id} cpus={server.cpus} />);
     return (
       <div>
-      {racks}
+      {servers}
       </div>
     );
   }
